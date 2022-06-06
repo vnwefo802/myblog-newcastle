9d6e67faf49fa8d34d8ecf9b672da41afacfd768
rangeprint <info@rangeprint.co.uk>
2022-06-06 15:22:48 +0100

    changes to admin pages

--- resources/views/admin_dashboard/comments/index.blade.php
+++ resources/views/admin_dashboard/comments/index.blade.php
@@ -4,25 +4,10 @@
 <div class="page-wrapper">
     <div class="page-content">
         <div class="container mx-auto bg-white rounded shadow dark:bg-gray-800">
-            <div class="flex flex-col items-start justify-between w-full p-8 lg:flex-row lg:items-stretch">
-                <div class="flex flex-col items-start justify-end w-full gap-2 lg:ml-24 lg:flex-row lg:items-center">
-                    <div class="flex flex-col items-start w-full lg:w-1/4 xl:w-1/3 lg:flex-row lg:items-center">
-                        <div class="relative w-full mb-2 lg:mb-0">
-                            <div class="absolute flex items-center h-full pl-4 text-gray-600 right-2 dark:text-gray-400">
-                                <button class="bg-gray-100 border-2 border-gray-100 rounded focus:bg-gray-300 focus:border-gray-300 active:bg-gray-200 active:border-gra-200">
-                                    <img class="dark:hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/advance_table_with_filters_search_and_two_level_action_buttons-svg1.svg" alt="search">
-                                    <img class="hidden dark:block" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/advance_table_with_filters_search_and_two_level_action_buttons-svg1dark.svg" alt="search">
-                                </button>
-                            </div>
-                            <label for="search" class="hidden"></label>
-                            <input placeholder="Search" id="search" class="flex items-center w-full h-10 pl-2 text-sm font-normal text-gray-600 bg-gray-100 border border-gray-300 rounded dark:bg-gray-700 dark:text-gray-400 focus:outline-none focus:border focus:border-indigo-700 dark:border-gray-200" placeholder="Search" />
-                        </div>
-                    </div>
-                    <button class="flex flex-row px-2 py-2 my-2 text-sm text-white transition duration-150 ease-in-out bg-indigo-700 border border-transparent rounded focus:shadow-outline-gray w-44 lg:w-24 lg:my-0 lg:ml-7 xl:ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hover:bg-indigo-600">
-                        <img class="px-2" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/advance_table_with_filters_search_and_two_level_action_buttons-svg1dark.svg" alt="search">search
-                    </button>
+            <div class="flex flex-col w-full pb-4 pt-8 lg:flex-row lg:items-stretch">
+                <div class="flex flex-col  w-full gap-2  lg:flex-row lg:items-center">
                     <a href="{{ route('admin.users.create') }}" class=" w-[162px] h-[38px]">
-                        <button class="py-2 text-sm text-white transition duration-150 ease-in-out bg-indigo-700 border border-transparent rounded focus:shadow-outline-gray lg:my-0 lg:ml-2 xl:ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hover:bg-indigo-600 w-[162px] h-[38px]" > Add New Post </button>
+                        <button class="py-2 text-sm text-white transition duration-150 ease-in-out bg-indigo-700 border border-transparent rounded focus:shadow-outline-gray focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hover:bg-indigo-600 w-[162px] h-[38px]" > Add New Post </button>
                     </a>
                 </div>
             </div>
