5aad80413461116196eeb86fcbc8b62e6f6011fe
rangeprint <info@rangeprint.co.uk>
2022-06-07 14:47:05 +0100

    changes to blog pages

--- resources/views/components/blog/form/textarea.blade.php
+++ resources/views/components/blog/form/textarea.blade.php
@@ -1,6 +1,6 @@
 @props(['name', 'placeholder', 'value'])
 
-<textarea rows="5" required id="{{ $name }}" name='{{ $name }}' class="form-control" placeholder="{{ $placeholder }}">{{ $value }}</textarea>
+<textarea rows="5" required id="{{ $name }}" name='{{ $name }}' class="form-control border border-gray-300 rounded px-2" placeholder="{{ $placeholder }}">{{ $value }}</textarea>
 
 @error($name)
 <small class="text-danger">{{ $message }}</small>
