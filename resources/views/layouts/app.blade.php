ce54a49bddf0fb900a6ed3eca2cd0eae64bdaf85
rangeprint <info@rangeprint.co.uk>
2022-06-08 12:57:51 +0100

    changes to admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,8 +56,8 @@
                     <!-- ============================================ -->
                     <!--                 Website Logo                 -->
                     <!-- ============================================ -->
-                    <a href="/" class="flex items-center" aria-label="NewcastleFoundationLogo">
-                        <img src="{{ asset('images/NewcastleFoundationLogo.jpg') }}" class="w-20 h-12" alt="Newcastle logo" aria-label="Newcastle logo" >
+                    <a href="/" class="flex items-center" aria-label="NewcastleFoundationLogo"> <!-- class="w-[163px] h-[55px]" -->
+                        <img src="{{ asset('images/NewcastleFoundationLogo.jpg') }}" class="w-[163px] h-[55px]"  alt="Newcastle logo" aria-label="Newcastle logo" >
                         {{-- <img src="{{ asset('storage/'.$home->logo) }}" class="w-20 h-12 mr-2 sm:h-20 sm:w-32" alt="Newcastle logo" aria-label="Newcastle logo" > --}}
 
 
