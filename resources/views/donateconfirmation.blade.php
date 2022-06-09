c97898ff2ee09003d2865ca56936585be64e26ff
Kyle <kylehardingactor@gmail.com>
2022-06-09 12:29:58 +0100

    donateconfirmation

--- resources/views/donateconfirmation.blade.php
+++ resources/views/donateconfirmation.blade.php
@@ -2,6 +2,8 @@
 
 @section('title', 'Donate')
 
+@section('content')
+
 <!-- Container for demo purpose -->
 <div class="container my-24 px-6 mx-auto">
 
@@ -9,57 +11,17 @@
     <section class="mb-32 text-gray-800 text-center">
       <div class="px-6 py-12 md:px-12">
         <div class="container mx-auto xl:px-32">
-          <div class="grid lg:grid-cols-2 flex items-center">
+          <div class=''>
             <div class="md:mt-12 lg:mt-0 mb-12 lg:mb-0">
               <div
                 class="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14"
                 style="background: hsla(0, 0%, 100%, 0.55); backdrop-filter: blur(30px)"
               >
                 <h2 class="text-3xl font-bold mb-12">
-                  Trusted by the best companies <br />
-                  <span class="">around the world</span>
+                  Thank You For Your Donation!
                 </h2>
-                <div class="grid md:grid-cols-2 gap-x-6">
-                  <div class="mb-12">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/new-templates/landing-page/logo6-1.png"
-                      class="grayscale px-6"
-                      alt="Nasa - logo"
-                    />
-                  </div>
-  
-                  <div class="mb-12">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/new-templates/landing-page/logo5-1.png"
-                      class="grayscale px-6"
-                      alt="Amazon - logo"
-                    />
-                  </div>
-  
-                  <div class="mb-12 md:mb-0">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/new-templates/landing-page/logo3-1.png"
-                      class="grayscale px-6"
-                      alt="Nike - logo"
-                    />
-                  </div>
-  
-                  <div class="">
-                    <img
-                      src="https://mdbootstrap.com/img/Photos/new-templates/landing-page/logo4-1.png"
-                      class="grayscale px-6"
-                      alt="Ikea - logo"
-                    />
-                  </div>
-                </div>
+                <p>We appreciate the support. This will help us accomplish our goals and for that we thank you.</p>
               </div>
-            </div>
-            <div class="md:mb-12 lg:mb-0">
-              <img
-                src="https://mdbootstrap.com/img/new/textures/full/166.jpg"
-                class="w-full rounded-lg shadow-lg"
-                alt=""
-              />
             </div>
           </div>
         </div>
