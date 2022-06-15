c5e4b321d3c1172e17582accc3114236689aefa2
rangeprint <info@rangeprint.co.uk>
2022-06-15 15:21:33 +0100

    change to donate

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -34,31 +34,38 @@
             <div class="shadow md:w-3/6 w-11/12 flex flex-col md:px-16 text-center border md:py-16 py-6 px-6 bg-white"
                 method="POST">
                 @csrf
+                <div class="grid grid-cols-2 grid-rows-1">
+                    <Button class="border-2 text-center border-gray-600 rounded-l-lg py-2" onclick="showoneoff()">ONE OFF</Button>
+                    <Button class="border-2 text-center border-gray-600 rounded-r-lg py-2" onclick="showmonthly()">MONTHLY</button>
+                </div>
+                <div class="" id="one_off">
+                    <h3 class="text-2xl font-light pb-4">Choose amount to donate</h3>
 
-                <h3 class="text-2xl font-light pb-4">Choose amount to donate</h3>
+                    <div class="donate-desc flex flex-col">
+                        <div class="relative mb-8 flex justify-center items-center text-2xl font-light">
+                            <div class="absolute left-3 text-blue-500">&#8358;</div>
+                            <input type="text" class="border grow pl-8 py-2 font-light w-full donate-amount" inputmode='numeric'
+                                pattern="[0-9.]*" autocomplete="" name="amount" id="">
+                        </div>
+                        <button class=" bg-blue-500 text-white px-3 py-2 donate-button flex justify-center">
+                            <svg id="donate-spinner" role="status"
+                                class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 hidden"
+                                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="currentColor" />
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentFill" />
+                            </svg>
+                            Donate Now
+                        </button>
 
-                <div class="donate-desc flex flex-col">
-                    <div class="relative mb-8 flex justify-center items-center text-2xl font-light">
-                        <div class="absolute left-3 text-blue-500">&#8358;</div>
-                        <input type="text" class="border grow pl-8 py-2 font-light w-full donate-amount" inputmode='numeric'
-                            pattern="[0-9.]*" autocomplete="" name="amount" id="">
                     </div>
-                    <button class=" bg-blue-500 text-white px-3 py-2 donate-button flex justify-center">
-                        <svg id="donate-spinner" role="status"
-                            class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 hidden"
-                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor" />
-                            <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill" />
-                        </svg>
-                        Donate Now
-                    </button>
+                </div>
+                <div id="monthly">
 
                 </div>
-
                 <form id="payment-form" class="py-5 my-4 hidden">
                     {{-- <div>
                     <h3 class="text-3xl mb-5 font-light">Donation Amount - <span class="text-blue-500">&#8358;
@@ -119,5 +126,22 @@
                     class="text-blue-500 underline">{{ $footer->contact_info }}</a></p>
         </div>
     </div>
+<script>
+    var x = document.getElementById("one_off");
+    var z = document.getElementById("monthly");
+
+    function showoneoff(){
+        x.style.display = "block"
+        z.style.display = "none"
+    }
+    function showmonthly(){
+        z.style.display = "block"
+        x.style.display = "none"
+    }
+
+
+
+
+</script>
 
 @endsection
