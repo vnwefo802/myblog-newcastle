72702b412b83b9bc001f9c1129b34e7d9cdadd3a
victory nwefo <vnwefo92@gmail.com>
2022-06-22 10:52:39 +0100

    added an image for the placceholder

--- resources/views/admin_dashboard/layouts/header.blade.php
+++ resources/views/admin_dashboard/layouts/header.blade.php
@@ -132,8 +132,7 @@ if (Auth::check()) {
                     </div>
                     <div class="user-box dropdown">
                         <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            
-                            <img src="{{ asset('admin_dashboard_assets/images/avatars/avatar-2.png') }}" class="user-img" alt="user avatar">
+                            <img src="{{ asset('images/user_placeholder.jpg') }}" class="user-img" alt="user avatar">
                             <div class="user-info ps-3">
                                 <p class="user-name mb-0">{{ Auth::user()->name }}</p>
                                 <p class="designattion mb-0">{{ Auth::user()->email }}</p>
